d7da79f156c05bda33ef3494b1e32cc199d915b3
mfreund <matth.freund@gmail.com>
2023-05-13 16:33:49 +0200

    Add firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBCFwHcB-6OfTnsJHY6-UxDgz1cKCRlgq4",
+    authDomain: "ocean-cookbook.firebaseapp.com",
+    projectId: "ocean-cookbook",
+    storageBucket: "ocean-cookbook.appspot.com",
+    messagingSenderId: "956768765625",
+    appId: "1:956768765625:web:5f003cb8a7fd2c978afd68"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
