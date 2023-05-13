4333ff5de90f2d6c43fc1d6256a0d33c912c6366
mfreund <matth.freund@gmail.com>
2023-05-13 17:18:20 +0200

    Minor change to firebase client
    Change based on information from
    https://www.youtube.com/watch?v=8NlUTFppJkU

--- src/lib/firebase/firebase.client.ts
+++ src/lib/firebase/firebase.client.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, setPersistence, inMemoryPersistence } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +15,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+let firebaseapp;
+if (getApps().length > 0) {
+    firebaseapp = getApp();
+    deleteApp(firebaseapp);
+}
+firebaseapp = initializeApp(firebaseConfig);
+
+export const auth = getAuth(firebaseapp);
