8f37d47a9929c42a9427d0f05070188f16665d5a
mfreund <matth.freund@gmail.com>
2023-05-25 18:18:06 +0200

    Start implementation of the recipe view

--- src/routes/recipes/[recipeId]/+page.svelte
+++ src/routes/recipes/[recipeId]/+page.svelte
@@ -6,4 +6,24 @@
 	let recipe: Recipe = data.recipe;
 </script>
 
-<h1>{recipe.name}</h1>
+<h3>{recipe.name}</h3>
+
+<img src={recipe.image} alt="recipeImage" />
+
+<br />
+<h5>Zutaten:</h5>
+<ul>
+	{#each recipe.ingredients as ingredient}
+		<li>{ingredient.count || ''}{ingredient.unit || ''}{ingredient.count || ingredient.unit ? ' ' : ''}{ingredient.name}</li>
+	{/each}
+</ul>
+
+<br />
+<h5>Zubereitung:</h5>
+{recipe.instructions}
+
+<style lang="scss">
+	img {
+		max-height: 10rem;
+	}
+</style>
