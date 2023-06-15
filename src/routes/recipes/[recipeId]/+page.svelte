9cef82b79c9e258a56465b38302f0056a72d5338
mfreund <matth.freund@gmail.com>
2023-06-15 17:32:51 +0200

    Allow changing recipe yield on recipe view

--- src/routes/recipes/[recipeId]/+page.svelte
+++ src/routes/recipes/[recipeId]/+page.svelte
@@ -4,15 +4,17 @@
 	import IconButton from '@smui/icon-button';
 	import formatQuantity from 'format-quantity';
 	import type { Ingredient } from 'parse-ingredient';
+
 	/** @type {import('./$types').PageData} */
 	export let data: any;
 
 	let recipe: Recipe = data.recipe;
+	let currentYield = recipe.recipeYield || 1;
 
 	const getIngredientString = (ingredient: Ingredient): string => {
 		let ret =
-				(ingredient.quantity != null ? formatQuantity(ingredient.quantity) : '') +
-				(ingredient.quantity2 != null ? '-' + formatQuantity(ingredient.quantity2) : '');;
+			(ingredient.quantity != null ? formatQuantity(ingredient.quantity) : '') +
+			(ingredient.quantity2 != null ? '-' + formatQuantity(ingredient.quantity2) : '');
 
 		if (ingredient.unitOfMeasure) {
 			ret += ingredient.unitOfMeasure;
@@ -24,14 +26,49 @@
 
 		return ret.trim();
 	};
+
+	const increaseYield = () => {
+		if (currentYield < 1) {
+			currentYield *= 2;
+		} else {
+			currentYield += 1;
+		}
+	};
+
+	const decreaseYield = () => {
+		if (currentYield <= 1) {
+			currentYield /= 2;
+		} else {
+			currentYield -= 1;
+		}
+	};
+
+	const getIngredientWithUpdatedCounts = (ingredient: Ingredient, intendedYield: number) => {
+		return {
+			...ingredient,
+			quantity: ingredient.quantity
+				? (ingredient.quantity / (recipe.recipeYield || 1)) * intendedYield
+				: null,
+			quantity2: ingredient.quantity2
+				? (ingredient.quantity2 / (recipe.recipeYield || 1)) * intendedYield
+				: null
+		};
+	};
+
+	$: ingredientsForCurrentYield = recipe.ingredients.map((ingredient) =>
+		getIngredientWithUpdatedCounts(ingredient, currentYield)
+	);
 </script>
 
-<h3>{recipe.name}</h3>
-<IconButton
-	class="material-icons"
-	aria-label="Edit"
-	on:click={() => goto(`/recipes/${recipe.id}/edit`)}>edit</IconButton
->
+<div class="headingContainer">
+	<h4>{recipe.name}</h4>
+	<IconButton
+		class="material-icons"
+		aria-label="Edit"
+		on:click={() => goto(`/recipes/${recipe.id}/edit`)}
+		>edit
+	</IconButton>
+</div>
 
 {#if (recipe.images || []).length > 0 && typeof recipe.images[0] === 'string'}
 	<div class="imagesContainer">
@@ -39,25 +76,57 @@
 	</div>
 {/if}
 
-<h5>Zutaten:</h5>
-<ul>
-	{#each recipe.ingredients as ingredient}
-		<li>
-			{getIngredientString(ingredient)}
-		</li>
-	{/each}
-</ul>
+<div class="ingredientsAndInstructionsContainer">
+	<div class="ingredientsContainer">
+		{#if recipe.recipeYield}
+			<h5>
+				Zutaten für
+				<IconButton class="material-icons" on:click={decreaseYield} size="button">remove</IconButton
+				>
+				{formatQuantity(currentYield)}
+				<IconButton class="material-icons" on:click={increaseYield} size="button">add</IconButton>
+				Portionen:
+			</h5>
+		{:else}
+			<h5>Zutaten</h5>
+		{/if}
+		<ul>
+			{#each ingredientsForCurrentYield as ingredient}
+				<li>
+					{getIngredientString(ingredient)}
+				</li>
+			{/each}
+		</ul>
+	</div>
 
-<div class="instructionsContainer">
-	<h5>Zubereitung:</h5>
-	{#each recipe.instructions as instruction}
-		<div class="instructionContainer">
-			{instruction}
-		</div>
-	{/each}
+	<div class="instructionsContainer">
+		<h5>Zubereitung:</h5>
+		{#each recipe.instructions as instruction}
+			<div class="instructionContainer">
+				{instruction}
+			</div>
+		{/each}
+	</div>
 </div>
 
+{#if recipe.comment}
+	<div class="commentContainer">
+		<h5>Kommentar:</h5>
+		{#each recipe.instructions as instruction}
+			<div class="instructionContainer">
+				{instruction}
+			</div>
+		{/each}
+	</div>
+{/if}
+
 <style lang="scss">
+	.headingContainer {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
 	img {
 		max-height: 10rem;
 		border-radius: 0.5rem;
@@ -66,6 +135,7 @@
 	h5 {
 		margin-top: 2rem;
 		margin-bottom: 0.5rem;
+		line-height: 2em;
 	}
 
 	ul {
@@ -76,7 +146,25 @@
 		margin-top: 1rem;
 	}
 
+	.ingredientsAndInstructionsContainer {
+		display: flex;
+		flex-direction: row;
+		align-items: flex-start;
+	}
+
+	@media (max-width: 800px) {
+		.ingredientsAndInstructionsContainer {
+			flex-direction: column;
+		}
+	}
+
+	.ingredientsContainer {
+		width: 25rem;
+		min-width: 25rem;
+	}
+
 	.instructionsContainer {
+		flex-grow: 1;
 		display: flex;
 		flex-direction: column;
 		gap: 0.5rem;
