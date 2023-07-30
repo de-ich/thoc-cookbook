d4393ede62eefa0dfe3456f3218685875392c17f
mfreund <matth.freund@gmail.com>
2023-07-30 13:40:04 +0200

    Display images in recipe list

--- src/components/RecipeCard.svelte
+++ src/components/RecipeCard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { PUBLIC_IMAGEKIT_STORAGE_URL } from '$env/static/public';
 	import type { RecipePreview } from '$lib/database/Recipe';
 
 	export let recipe: RecipePreview;
@@ -7,7 +8,16 @@
 <div class="recipeCardContainer">
 	<a href={'/recipes/' + recipe.id}>
 		<div class="recipeCard">
-			{recipe.name}
+			<div class="recipeImageContainer">
+				{#if (recipe.previewImage)}
+					<img
+						src={PUBLIC_IMAGEKIT_STORAGE_URL + 'tr:h-80/' + recipe.previewImage}
+						alt="recipePreviewImage"
+						loading="lazy"
+					/>
+				{/if}
+			</div>
+			<div><span class="recipeTitle">{recipe.name}</span></div>
 		</div>
 	</a>
 </div>
@@ -15,16 +25,41 @@
 <style lang="scss">
 	.recipeCardContainer {
 		width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
 	}
 
 	.recipeCard {
 		background-color: var(--mdc-theme-background);
-		border: 0.05rem solid var(--mdc-theme-secondary);
 		border-radius: 0.5rem;
-		padding: 0.5rem;
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
+		align-items: center;
 
 		&:hover {
 			color: var(--mdc-theme-primary);
+			background-color: var(--mdc-theme-secondary);
+		}
+
+		.recipeImageContainer {
+			height: 5rem;
+			width: 8.89rem;
+			border-radius: 0.5rem;
+			background-color: var(--mdc-theme-primary);
+			flex-shrink: 0;
+
+			img {
+				height: 5rem;
+				width: 8.89rem;
+				object-fit: cover;
+				border-radius: 0.5rem;
+			}
+		}
+
+		span.recipeTitle {
+			font-weight: 500;
 		}
 	}
 </style>
