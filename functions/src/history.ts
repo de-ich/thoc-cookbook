5a82195540fb79052bdd836c3af4a0e8ecc50453
mfreund <matth.freund@gmail.com>
2024-05-20 17:32:35 +0200

    Fix for updating the history document

--- functions/src/history.ts
+++ functions/src/history.ts
@@ -18,14 +18,27 @@ export const pushToHistory = onCall({ maxInstances: 1 }, async (request) => {
 
     const historyDocRef = db.doc(`aggregates/history`);
 
-    historyDocRef.get().
-        then(snapshot => snapshot.data() as History).
-        then(history => {
-            var newEntries = history.entries.filter(entry => entry.recipeId !== recipeId);
-            newEntries.unshift({"recipeId": recipeId, "timestamp": Timestamp.now()});
-            return newEntries.slice(0, 10);
-        }).
-        then(newEntries => {
-            historyDocRef.set({entries: newEntries});
-        });    
+    let history : History = {
+        entries: []
+    };
+    
+    try {
+        history = await historyDocRef.get().then(snapshot => snapshot.data() as History);
+        if (typeof(history.entries) !== 'object') {
+            history.entries = [];
+        }
+    } catch (e) {
+        console.error("Unable to convert document 'aggregates/history' to an instance of History. " +
+        "The following error occurred: " + e);
+    }
+    
+    console.log(`Old history: ${history}`);
+
+    var newEntries = history.entries.filter(entry => entry.recipeId !== recipeId);
+    newEntries.unshift({"recipeId": recipeId, "timestamp": Timestamp.now()});
+    history.entries = newEntries.slice(0, 10);
+
+    console.log(`New entries: ${history}`);
+    
+    await historyDocRef.set(history);
 });
