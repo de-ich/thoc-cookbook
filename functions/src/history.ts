9c9b4ef8bf886297f1879a0dc80f279f58289cc8
mfreund <matth.freund@gmail.com>
2024-07-20 16:40:19 +0200

    Do not limit history to 10 entries

--- functions/src/history.ts
+++ functions/src/history.ts
@@ -32,13 +32,9 @@ export const pushToHistory = onCall({ maxInstances: 1 }, async (request) => {
         "The following error occurred: " + e);
     }
     
-    console.log(`Old history: ${history}`);
-
     var newEntries = history.entries.filter(entry => entry.recipeId !== recipeId);
     newEntries.unshift({"recipeId": recipeId, "timestamp": Timestamp.now()});
-    history.entries = newEntries.slice(0, 10);
+    history.entries = newEntries
 
-    console.log(`New entries: ${history}`);
-    
     await historyDocRef.set(history);
 });
