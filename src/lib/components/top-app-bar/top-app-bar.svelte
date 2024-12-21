a828661c1400a7e95a9544fc27889638b894dc88
mfreund <matth.freund@gmail.com>
2024-12-21 14:32:24 +0100

    Minor change to the header for mobile

--- src/lib/components/top-app-bar/top-app-bar.svelte
+++ src/lib/components/top-app-bar/top-app-bar.svelte
@@ -4,7 +4,7 @@
 			<slot />
 		</div>
 		<div
-			class="h-2 w-full bg-primary xl:max-w-screen-2xl xl:bg-gradient-to-r xl:from-background xl:via-primary xl:to-background"
+			class="h-2 w-full bg-primary xl:max-w-screen-2xl bg-gradient-to-r from-background via-primary to-background"
 		></div>
 	</div>
 </header>
